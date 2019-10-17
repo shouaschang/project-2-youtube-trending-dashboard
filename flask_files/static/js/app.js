cb9f6adcf536570e88feab11ec6f3d7e595299c8
DavisOberle <47678044+DavisOberle@users.noreply.github.com>
2019-10-17 18:27:43 -0500

    Update app.js

--- flask_files/static/js/app.js
+++ flask_files/static/js/app.js
@@ -1,56 +1,38 @@
-function buildMetadata(sample) {
+function buildBar1() {
+    var trace = {
+      type: "bar",
+      x: ["Music", "Sports", "Politics"],
+      y: [150, 200, 175]
 
-  // @TODO: Complete the following function that builds the metadata panel
+    };
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+    var data = [trace];
 
-    // Use `.html("") to clear any existing metadata
+    var layout = {
+     title: "Comments Comparison in All Categories",
+    };
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+    Plotly.newPlot('bar1', data, layout)
+  };
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-}
+buildBar1();
 
-function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+function buildBar2() {
+    var trace = {
+      type: "bar",
+      x: ["Music", "Sports", "Politics"],
+      y: [200, 175, 150]
 
-    // @TODO: Build a Bubble Chart using the sample data
+    };
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-}
+    var data = [trace];
 
-function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+    var layout = {
+     title: "Unique Videos Within Category",
+    };
 
-  // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
+    Plotly.newPlot('bar2', data, layout)
+  };
 
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-}
-
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
-}
-
-// Initialize the dashboard
-init();
+buildBar2();
