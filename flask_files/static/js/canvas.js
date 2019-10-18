1d02ac3397c7ffe4c0bd2972f18554f52d8570e2
shouaschang <36749672+shouaschang@users.noreply.github.com>
2019-10-17 23:44:56 -0500

    Updating file

--- flask_files/static/js/canvas.js
+++ flask_files/static/js/canvas.js
@@ -1,23 +1,21 @@
 window.onload = function () {
-
   var chart = new CanvasJS.Chart("chartContainer", {
-    theme: "light2", // "light2", "dark1", "dark2"
-    animationEnabled: true, // change to true		
-    title:{
-      text: "Unique view per Category"
-    },
-    data: [
-    {
-      // Change type to "bar", "area", "spline", "pie",etc.
-      type: "column",
-      dataPoints: [
-        { label: "Music",  y: 10  },
-        { label: "Sports", y: 15  },
-        { label: "Politics", y: 25  }
-      ]
-    }
-    ]
-  });
-  chart.render();
-  
-  }
+     theme: "light2", // "light2", "dark1", "dark2"
+      animationEnabled: true, // change to true
+       title:{
+           text: "Unique Videos per Category"
+          },
+           data: [
+              {
+  // Change type to "bar", "area", "spline", "pie",etc.
+          type: "bar",
+          dataPoints:
+            { label: "Music",  y: 6325  },
+            { label: "Sports", y: 2241  },
+            { label: "Politics", y: 2489  }
+          ]
+        }
+        ]
+      });
+chart.render();
+}
